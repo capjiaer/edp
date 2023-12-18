313b30a91dbaaea54ecd1c206761e078c1d2d4d4
chenanping <anping.chen@joinsilicon.com>
2023-12-18 17:48:07 +0800

    update main_proc

--- flow/packages/tcl/main_proc.tcl
+++ flow/packages/tcl/main_proc.tcl
@@ -1,7 +1,7 @@
 proc get_proc_info {tag flow_path {return_proc_info 1}} {
 	# This proc works for deliver required procs for each sub flow.
 	set script_path [file normalize [info script]]
-	set proc_path [file join $flow_path "initialize/cmds" $tag]
+	set proc_path [file join $flow_path "packages/tcl" $tag]
 	puts "Get proc info from $proc_path"
 	set file_list [glob -nocomplain -type f [file join $proc_path *]]
 	if {[llength $file_list]} {
