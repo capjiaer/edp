4be006e12227c4c9ffb66edc3c6b166aea7da969
chenanping <anping.chen@joinsilicon.com>
2023-12-18 17:44:40 +0800

    tcl_env update

--- flow/initialize/templates/main_util/tcl_env.tcl
+++ flow/initialize/templates/main_util/tcl_env.tcl
@@ -2,7 +2,7 @@ set script_path [file normalize [info script]]
 regexp -nocase {(.*cmds)/(.*)} $script_path matched_info cmds_path rest_info
 set main_path [file dirname $cmds_path]
 set tool_info [file tail [file dirname $script_path]]
-set tcl_package [file normalize [file join $main_path "../flow/packages/tcl/$tool_info"]]
+set tcl_package [file normalize [file join $main_path "flow/packages/tcl/$tool_info"]]
 set env_tcl [file normalize [file join $main_path "config/full.tcl"]]
 source [file dirname $tcl_package]/main_proc.tcl
 get_proc_info $tool_info $main_path/flow
