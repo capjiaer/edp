eb43c81162f3221e184347b960639805e912d9eb
chenanping <anping.chen@joinsilicon.com>
2023-12-18 17:32:06 +0800

    update tcl_env setup

--- flow/initialize/templates/main_util/tcl_env.tcl
+++ flow/initialize/templates/main_util/tcl_env.tcl
@@ -1,13 +1,11 @@
 set script_path [file normalize [info script]]
-regexp -nocase {(.*cmds)/(.*)} $script_path matched_info flow_path rest_info
-set tcl_package [file normalize [file join $flow_path "../flow/packages/tcl/pnr_innovus"]]
-set env_tcl [file normalize [file join $flow_path "../config/full.tcl"]]
-source "$tcl_package/main.tcl"
-
+regexp -nocase {(.*cmds)/(.*)} $script_path matched_info cmds_path rest_info
+set main_path [file dirname $cmds_path]
+set tool_info [file tain [file dirname $script_path]]
+set tcl_package [file normalize [file join $flow_path "../flow/packages/tcl/$tool_info"]]
+set env_tcl [file normalize [file join $main_path "config/full.tcl"]]
+source [file dirname $tcl_package]/main_proc.tcl
 lappend auto_path $tcl_package
-#package require file_edit_func
-#package require data_func
-
 # Initialize finished
 source $env_tcl
 
--- flow/packages/tcl/main_proc.tcl
+++ flow/packages/tcl/main_proc.tcl
@@ -1,13 +1,15 @@
-proc get_proc_info {tag} {
+proc get_proc_info {tag flow_path {return_proc_info 1}} {
 	# This proc works for deliver required procs for each sub flow.
 	set script_path [file normalize [info script]]
-	regexp -nocase {(.*flow)/(.*)} $script_path matched_info flow_path rest_info 
-	set proc_path [file join $flow_path "initialize/cmds" $tag "proc"]
+	set proc_path [file join $flow_path "initialize/cmds" $tag]
 	puts "Get proc info from $proc_path"
 	set file_list [glob -nocomplain -type f [file join $proc_path *]]
-	return file_list
-	foreach ele $file_list {
-		source $ele
+	if {[llength $file_list]} {
+		foreach ele $file_list {
+			if {$return_proc_info} {
+				puts "Read in proc $ele"
+			}
+			source $ele
+		}
 	}
 }
-
